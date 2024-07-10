77eb308b1ec9c71be4c7af6b523a482b50ea0614
황세웅 <0cookieboy0@gmail.com>
2024-07-10 09:52:34 +0900

    repository & service

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long id = memberService.join(member);
+
+        // then
+        Member findedMember = memberRepository.findOne(id);
+        assertThat(findedMember).isEqualTo(member);
+    }
+
+    @Test
+    void 중복회원예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("이미 존재하는 회원입니다.");
+    }
+}
