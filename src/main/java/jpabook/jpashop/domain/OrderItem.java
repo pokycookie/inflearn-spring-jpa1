cdb1d37397c54b93d4bf6ce62a59a6335d441a93
황세웅 <0cookieboy0@gmail.com>
2024-07-10 17:52:53 +0900

    order

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,14 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    // 비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
+
 }
